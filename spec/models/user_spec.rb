d7dd4efc371127a32b144e890e8346d04d5d40b3
Aditya <choppaaditya@gmail.com>
2012-05-10 03:33:49 +0530

    User model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  
+  it "should require a name" do
+  	no_name_user = User.new(:email => "user@xyz")
+  	no_name_user.should_not be_valid
+  end		
+
+  it "should require an email" do
+  	no_email_user = User.new(:name => "Example")
+  	no_email_user.should_not be_valid
+  end
 end
